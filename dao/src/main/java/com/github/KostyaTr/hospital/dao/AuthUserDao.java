b23981ace879ec00e9b5a82fda2b61180358d3d5
KostyaTr <kit.blackberry0408@gmail.com>
2020-04-09 23:47:24 +0300

    refactored AuthUserDao and AuthUserDao.java to work with database

--- dao/src/main/java/com/github/KostyaTr/hospital/dao/AuthUserDao.java
+++ dao/src/main/java/com/github/KostyaTr/hospital/dao/AuthUserDao.java
@@ -5,5 +5,5 @@ import com.github.KostyaTr.hospital.model.AuthUser;
 public interface AuthUserDao {
     AuthUser getByLogin(String login);
 
-    void saveAuthUser(AuthUser user);
+    Long saveAuthUser(AuthUser user);
 }
--- dao/src/main/java/com/github/KostyaTr/hospital/dao/impl/DefaultAuthUserDao.java
+++ dao/src/main/java/com/github/KostyaTr/hospital/dao/impl/DefaultAuthUserDao.java
@@ -1,51 +1,60 @@
 package com.github.KostyaTr.hospital.dao.impl;
 
-import com.github.KostyaTr.hospital.model.Role;
 import com.github.KostyaTr.hospital.dao.AuthUserDao;
+import com.github.KostyaTr.hospital.dao.DataSource;
 import com.github.KostyaTr.hospital.model.AuthUser;
+import com.github.KostyaTr.hospital.model.Role;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.*;
+
 
 public class DefaultAuthUserDao implements AuthUserDao {
-    private Map<String, AuthUser> userByLogin;
 
-    public DefaultAuthUserDao() {
-        this.userByLogin = new HashMap<>();
-        this.userByLogin.putIfAbsent("Virologist",
-                new AuthUser("Virologist", "Virologist", Role.Doctor));
-        this.userByLogin.putIfAbsent("Therapist",
-                new AuthUser("Therapist", "Therapist", Role.Doctor));
-        this.userByLogin.putIfAbsent("Surgeon",
-                new AuthUser("Surgeon", "Surgeon", Role.Doctor));
-        this.userByLogin.putIfAbsent("User",
-                new AuthUser("User", "User", Role.AuthorizedUser));
-        this.userByLogin.putIfAbsent("User01",
-                new AuthUser("User01", "User01", Role.AuthorizedUser));
+    private static class SingletonHolder{
+        final static AuthUserDao INSTANCE_HOLDER = new DefaultAuthUserDao();
     }
 
-    private static volatile AuthUserDao instance;
+    public static AuthUserDao getInstance(){return DefaultAuthUserDao.SingletonHolder.INSTANCE_HOLDER;}
 
-    public static AuthUserDao getInstance() {
-        AuthUserDao localInstance = instance;
-        if (localInstance == null) {
-            synchronized (AuthUserDao.class) {
-                localInstance = instance;
-                if (localInstance == null) {
-                    instance = localInstance = new DefaultAuthUserDao();
-                }
-            }
-        }
-        return localInstance;
-    }
 
     @Override
     public AuthUser getByLogin(String login) {
-        return userByLogin.get(login);
+        try (Connection connection = DataSource.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from auth_user where login = ?")) {
+            preparedStatement.setString(1, login);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return new AuthUser(
+                            resultSet.getLong("id"),
+                            resultSet.getString("login"),
+                            resultSet.getString("password"),
+                            Role.valueOf(resultSet.getString("role")),
+                            resultSet.getLong("user_id"));
+                } else {
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void saveAuthUser(AuthUser user) {
-        userByLogin.putIfAbsent(user.getLogin(), user);
+    public Long saveAuthUser(AuthUser user) {
+        final String sql = "insert into auth_user(login, password, role, user_id) values(?,?,?,?)";
+        try (Connection connection = DataSource.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, user.getLogin());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getRole().name());
+            preparedStatement.setLong(4, user.getUserId());
+            preparedStatement.executeUpdate();
+            try (ResultSet key = preparedStatement.getGeneratedKeys()) {
+                key.next();
+                return key.getLong(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
