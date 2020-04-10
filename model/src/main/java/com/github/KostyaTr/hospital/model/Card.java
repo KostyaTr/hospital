b500a7890fb56110dcda07f3cb209db0b2f6fb60
KostyaTr <kit.blackberry0408@gmail.com>
2020-04-10 22:13:27 +0300

    refactored model

--- model/src/main/java/com/github/KostyaTr/hospital/model/Card.java
+++ model/src/main/java/com/github/KostyaTr/hospital/model/Card.java
@@ -1,5 +1,39 @@
 package com.github.KostyaTr.hospital.model;
 
-public class Card {
+import java.util.Date;
 
+public class Card {
+    private Long cardId;
+    private Long userId;
+    private String history;
+    private Date birthday;
+    private boolean insurance;
+
+    public Card(Long cardId, Long userId, String history, Date birthday, boolean insurance) {
+        this.cardId = cardId;
+        this.userId = userId;
+        this.history = history;
+        this.birthday = birthday;
+        this.insurance = insurance;
+    }
+
+    public Long getCardId() {
+        return cardId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public boolean isInsurance() {
+        return insurance;
+    }
 }
