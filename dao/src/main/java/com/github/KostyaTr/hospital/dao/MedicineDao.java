d78f90779dc7bb01df4e071c32988beeb0b22edc
KostyaTr <kit.blackberry0408@gmail.com>
2020-04-11 13:32:31 +0300

    added function to display all medicine

--- dao/src/main/java/com/github/KostyaTr/hospital/dao/MedicineDao.java
+++ dao/src/main/java/com/github/KostyaTr/hospital/dao/MedicineDao.java
@@ -2,7 +2,11 @@ package com.github.KostyaTr.hospital.dao;
 
 import com.github.KostyaTr.hospital.model.Medicine;
 
+import java.util.List;
+
 public interface MedicineDao {
+    List<Medicine> getMedicine();
+
     Long addMedicine(Medicine medicine);
 
     boolean updateStockBalanceById(Integer stockBalance, Long medicineId);
--- dao/src/main/java/com/github/KostyaTr/hospital/dao/impl/DefaultMedicineDao.java
+++ dao/src/main/java/com/github/KostyaTr/hospital/dao/impl/DefaultMedicineDao.java
@@ -5,6 +5,8 @@ import com.github.KostyaTr.hospital.dao.MedicineDao;
 import com.github.KostyaTr.hospital.model.Medicine;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DefaultMedicineDao implements MedicineDao {
     private final int ONE_ROW_AFFECTED = 1;
@@ -52,5 +54,31 @@ public class DefaultMedicineDao implements MedicineDao {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public List<Medicine> getMedicine() {
+        final String sql = "select * from medicine;";
+
+        try (Connection connection = DataSource.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            List<Medicine> medicines = new ArrayList<>();
+            while (resultSet.next()){
+                final Medicine user = new Medicine(
+                        resultSet.getLong("id"),
+                        resultSet.getString("medicine_name"),
+                        resultSet.getDouble("normal_dose"),
+                        resultSet.getDouble("critical_dose"),
+                        resultSet.getInt("package_amount"),
+                        resultSet.getDouble("price"),
+                        resultSet.getInt("stock_balance"));
+                medicines.add(user);
+            }
+            return medicines;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
