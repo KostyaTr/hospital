b305e7632c0f5857e39d3fd5c8f90d453f05cf30
KostyaTr <kit.blackberry0408@gmail.com>
2020-04-13 02:38:41 +0300

    clean up

--- web/src/main/java/com/github/KostyaTr/hospital/web/WebUtils.java
+++ web/src/main/java/com/github/KostyaTr/hospital/web/WebUtils.java
@@ -8,9 +8,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public abstract class WebUtils {
+
     private WebUtils() {
     }
     private static final Logger log = LoggerFactory.getLogger(WebUtils.class);
@@ -32,19 +32,5 @@ public abstract class WebUtils {
         } else {
             return "guestPage";
         }
-    }
-
-    public static Patient userToPatient(User user, String appointedDoctor, String visitingTime){
-        return new Patient(user.getLogin(), user.getFirstName(), user.getLastName(),
-                user.getEmail(), user.getEmail(), appointedDoctor, visitingTime);
-    }
-
-    public static boolean makeAppointmentCheck(String doctorName, List<MedDoctor> doctors){
-        for (MedDoctor doctor : doctors) {
-            if ((doctor.getFirstName() + " " + doctor.getLastName()).equals(doctorName)){
-                return true;
-            }
-        }
-        return false;
     }
 }
